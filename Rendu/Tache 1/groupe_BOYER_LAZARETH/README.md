69677037d142c1ead7959b309ddf800a62704130
charles.boyer <63152241+charlesboyerCPE@users.noreply.github.com>
2020-11-16 10:43:10 +0100

    Add files via upload

---
+++ Rendu/Tache 1/groupe_BOYER_LAZARETH/README.md
@@ -0,0 +1,49 @@
+# Projet: Algorithmes et C appliqués aux Systèmes Numériques
+
+## Objectif
+
+L'objectif du projet est de créer un environnement client-serveur pour l'analyse et la gestion desimages et de leurs métadonnées. Cependant, contrairement aux applications client-serveurtraditionnelles, nous n'enverrons pas les images au serveur pour analyse, mais nous effectuerons une analyse d'image côté client et enverrons des rapports de synthèse au serveur.
+L'objectif de cette première tâche est de se familiariser avec le code et de manipuler les différents types de messages qui seront transmits.
+
+## Fichiers
+
+- src/
+  - bmp.c
+  - bmp.h
+  - client.c
+  - client.h
+  - serveur.c
+  - serveur.h
+  - couleur.c
+  - couleur.h
+- CONTRIBUTORS.md
+- INSTALL.md
+- MakeFile
+- README.md
+
+## Bibliothèques
+
+sys/types.h
+sys/socket.h
+sys/epoll.h
+netinet/in.h
+stdio.h
+stdlib.h
+string.h
+unistd.h
+
+## Références
+
+- Groupe_BOURDIER_FIGUET pour quelques questions
+- StackOverflow
+
+## Difficultés
+
+Charles BOYER:
+Mon travail dans cette tâche a été d'effectué les questions b et d, ainsi que l'ajout de commentaires Doxygen.
+Travaillant actuellement sur un projet personnel comportant des communications client-serveur en C++, le compréhension du code a été assez rapide.
+Les réels soucis que j'ai rencontré ont été au niveau de la compréhension des questions.
+
+Baptiste LAZARETH:
+Ma tâche a été de réaliser les question a, c, e. Pas très à l'aise avec le C.
+J'ai trouvé la tâche 1 de difficulté moyenne.
---
+++ Rendu/Tache 1/groupe_BOYER_LAZARETH/src/serveur.h
@@ -0,0 +1,91 @@
+/*!
+ * \file serveur.h
+ * \brief Fichier contenant les prototypes des fonctions
+ *      du programme serveur
+ * \author John Samuel
+ * \copyright 2020 John Samuel GPL-3.0-or-later
+ * \version 1.0
+ * \date 2020
+ */
+
+#ifndef __SERVER_H__
+#define __SERVER_H__
+
+/*!
+ * \def PORT 8089
+ */
+#define PORT 8089
+
+/*!
+ * \fn int recois_envoie_message(int socketfd)
+ * \brief Fonction permettant l'envoi et la reception de message.
+ *      La fonction accepte la connexion du client et lis les données envoyées
+ *      par ce dernier. La fonction envoi ensuite un message en retour.
+ * \param [in] socketFd: l'identifiant de la socket
+ * 
+ * \return 0 si tout a bien fonctionner, -1 si il y a eu une erreur
+ */
+int recois_envoie_message(int socketfd);
+
+/*!
+ * \fn void plot(char *data)
+ * \brief Procédure permettant d'ouvir une fenêtre GNUPlot affichant les couleurs
+ * \param [in] data: Pointeur vers un tableau contenant les couleurs d'une image
+ */
+void plot(char *data);
+
+/*!
+ * \fn int renvoi_nom(int client_socket_fd, char *data)
+ * \brief Fonction permettant de renvoyer le nom au client
+ * \param [in] client_socket_fd: l'identifiant de la socket de dialogue avec le client
+ * \param [in] data: Pointeur vers un tableau contenant le message à renvoyer
+ * 
+ * \return 0 si tout a bien fonctionner, -1 si il y a eu une erreur
+ */
+int renvoi_nom(int client_socket_fd, char *data);
+
+/*!
+ * \fn int recois_numeros_calcule(int socketfd)
+ * \brief Fonction permettant de recevoir un opérateur et deux nombre.
+ *      La fonction calcul l'opération et envoi le résultat au client
+ * \param [in] client_socket_fd: l'identifiant de la socket de dialogue avec le client
+ * \param [in] data: le buffer contenant le message recu
+ * 
+ * \return 0 si tout a bien fonctionner, -1 si il y a eu une erreur
+ */
+int recois_numeros_calcule(int client_socket_fd, char *data);
+
+/*!
+ * \fn int renvoie_message(int client_socket_fd, char *data)
+ * \brief Fonction permettant de renvoyer un message au client
+ * \param [in] client_socket_fd: l'identifiant de la socket de dialogue avec le client
+ * \param [in] data: POinteur vers un tableau contenant le message à envoyer
+ * 
+ * \return 0 si tout a bien fonctionner, -1 si il y a eu une erreur
+ */
+int renvoie_message(int client_socket_fd, char *data);
+
+/*!
+ * \fn int renvoie_message(int client_socket_fd, char *data)
+ * \brief Fonction permettant de recevoir les couleurs d'une image.
+ *      La fonction sauvegarde le message dans un fichier txt.
+ * \param [in] client_socket_fd: l'identifiant de la socket de dialogue avec le client
+ * \param [in] data: Buffer contenant le message à envoyer
+ * 
+ * \return 0 si tout a bien fonctionner, -1 si il y a eu une erreur
+ */
+int recois_couleurs(int client_socket_fd, char *data);
+
+/*!
+ * \fn int recois_balises(int socketfd)
+ * \brief Fonction permettant de recevoir des balise en provenance du client.
+ *      La fonction enregistre les balises dans un fichier et envoi un message au client
+ *      pour lui indiquer que les balises ont étaient enregistrés.
+ * \param [in] client_socket_fd: l'identifiant de la socket de dialogue avec le client
+ * \param [in] data: le buffer contenant le message recu
+ * 
+ * \return 0
+ */
+int recois_balises(int client_socket_fd, char *data);
+
+#endif
